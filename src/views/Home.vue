0e42ac77ffc3bb42ed0c1e0c0196d8187111d098
slTrust <trustfor@sina.cn>
2020-10-14 15:12:13 +0800

    初始化 aside 的 html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,10 @@
   <div class="banner">
     <h1>轱辘UI</h1>
     <h2>一个厉害的 UI 框架</h2>
+    <p class="actions">
+      <a href="https://github.com">GitHub</a>
+      <router-link to="/doc">开始</router-link>
+    </p>
   </div>
 
 </div>
