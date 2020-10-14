b16081163551ec8fb5eeccf36965abafee98d637
slTrust <trustfor@sina.cn>
2020-10-14 23:35:56 +0800

    渲染 Tabs 内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,13 +1,13 @@
 <template>
   <template>
     <div>Tabs 示例</div>
-  </template> 
-<div>Tabs 示例</div>
-<h1>示例1</h1>
-<Tabs>
-  <Tab title="导航1">内容1</Tab>
-  <Tab title="导航2">内容2</Tab>
-</Tabs>
+  </template>
+  <div>Tabs 示例</div>
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
 
 <script lang="ts">
