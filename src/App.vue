2a9f984729df48f8cdf829bec021ad6cb5b393b4
slTrust <trustfor@sina.cn>
2020-10-14 15:41:18 +0800

    实现 aside 切换 - provide/inject 使用

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,16 @@
 <router-view></router-view>
 </template>
 
-<script>
+<script lang="ts">
+import {
+  ref,
+  provide
+} from 'vue'
 export default {
-  name: 'App'
+  name: 'App',
+  setup() {
+    const menuVisible = ref(false)
+    provide('menuVisible', menuVisible) // app根组件设置一个值 ，其他组件通过 inject可以获得值
+  }
 }
 </script>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,7 @@
 <div>
   <Topnav />
   <div class="content">
-    <aside>
+    <aside v-if="menuVisible">
       <h2>组件列表</h2>
       <ol>
         <li>
@@ -28,14 +28,22 @@
 
 <script lang="ts">
 import Topnav from '../components/Topnav.vue'
+import {
+  inject,
+  Ref
+} from 'vue';
 
 export default {
+  setup() {
+    const menuVisible = inject < Ref < boolean >> ('menuVisible') // get
+    return {
+      menuVisible
+    }
+  },
   components: {
     Topnav
   }
 }
-</script>
-
 </script>
 
 <style lang="scss" scoped>
