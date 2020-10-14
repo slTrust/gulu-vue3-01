31567506e7c9107183a3ca347be55c97933e4085
slTrust <trustfor@sina.cn>
2020-10-14 20:52:13 +0800

    创建 button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,23 @@
 <template>
-<div>
-  Button 示例
-</div>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
+  <div :size="size">
+    <Button @click="onClick"
+            @focus="onClick"
+            @mouseover="onClick">你好</Button>
+  </div>
 </template>
+<script lang="ts">
+import Button from '../lib/Button.vue'
+export default {
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      alert(1)
+    }
+    return {
+      onClick,
+    }
+  },
+}
+</script> 
---
+++ src/lib/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button v-bind="rest">
+      <slot />
+    </button>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  // 继承属性改为 false, 这样 div就不会再有 外面绑定的事件了
+  inheritAttrs: false,
+  setup(porps, context) {
+    const { size, ...rest } = context.attrs
+    return { size, rest }
+  },
+}
+</script>
+</script>
+<style lang="scss" scoped>
+div {
+  border: 1px solid red;
+}
+</style> 
