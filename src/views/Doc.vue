0e38fe76e9ebcf66831af0e672136ea6c5340b47
slTrust <trustfor@sina.cn>
2020-10-14 21:28:53 +0800

    支持 theme 属性

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -19,70 +19,53 @@
           </li>
         </ol>
       </aside>
-
       <main>
         <router-view />
       </main>
-
     </div>
-
   </div>
 </template>
-
 <script lang="ts">
 import Topnav from '../components/Topnav.vue'
 import { inject, Ref } from 'vue'
-
 export default {
+  components: { Topnav },
   setup() {
     const menuVisible = inject<Ref<boolean>>('menuVisible') // get
-    return {
-      menuVisible,
-    }
-  },
-  components: {
-    Topnav,
+    return { menuVisible }
   },
 }
 </script>
-
 <style lang="scss" scoped>
 .layout {
   display: flex;
   flex-direction: column;
   height: 100vh;
-
   > .nav {
     flex-shrink: 0;
   }
-
   > .content {
     flex-grow: 1;
     padding-top: 60px;
     padding-left: 156px;
-
     @media (max-width: 500px) {
       padding-left: 0;
     }
   }
 }
-
 .content {
   display: flex;
-
   > aside {
     flex-shrink: 0;
   }
-
   > main {
     flex-grow: 1;
     padding: 16px;
-    background: lightgreen;
+    background: white;
   }
 }
-
 aside {
-  background: white;
+  background: lightblue;
   width: 150px;
   padding: 16px;
   position: fixed;
@@ -90,18 +73,15 @@ aside {
   left: 0;
   padding-top: 70px;
   height: 100%;
-
   > h2 {
     margin-bottom: 4px;
   }
-
   > ol {
     > li {
       padding: 4px 0;
     }
   }
 }
-
 main {
   overflow: auto;
 }
