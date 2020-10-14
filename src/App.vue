c69e05ed6cb6439e165024177ff4680322a45920
slTrust <trustfor@sina.cn>
2020-10-14 14:05:51 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Aaa/>
+</template>
+
+<script>
+import Aaa from './components/Aaa.vue'
+
+export default {
+    name: 'App',
+    components: {
+      Aaa
+    }
+}
+</script>
