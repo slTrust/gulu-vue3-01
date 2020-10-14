da09ac7e1b2ecd60e85ddc421bbc7cf4b23b6c3b
slTrust <trustfor@sina.cn>
2020-10-14 19:44:02 +0800

    可以接受 value 和 input 事件

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,46 +1,51 @@
 <template>
-  <button @click="toggle" :class="{checked}"> <span></span> </button>
+  <button @click="toggle"
+          :class="{checked:value }"> <span></span> </button>
+  <div>{{value}}</div>
 </template>
 <script lang="ts">
 import { ref } from 'vue'
 export default {
-  setup(){
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
     const checked = ref(false)
-    const toggle = ()=>{
-      checked.value = !checked.value
+    const toggle = () => {
+      context.emit('input', !props.value)
     }
-    return {checked, toggle}
-  } 
+    return { checked, toggle }
+  },
 }
 </script>
 <style lang="scss">
-  $h: 22px;
-  $h2: $h - 4px;
-  button{
-    height: $h;
-    width: $h*2;
-    border: none;
-    background: grey;
-    border-radius: $h/2;
-    position: relative;
-  }
-  span{
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background:white;
-    border-radius: $h2 / 2;
-    transition: left 250ms;
-  } 
-  button.checked{
-    background: blue;
-  }
-  button.checked > span {
-    left: calc(100% - #{$h2} - 2px);
-  }
-  button:focus{
-    outline: none;
-  }
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: grey;
+  border-radius: $h/2;
+  position: relative;
+}
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h2 / 2;
+  transition: left 250ms;
+}
+button.checked {
+  background: blue;
+}
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+button:focus {
+  outline: none;
+}
 </style>
