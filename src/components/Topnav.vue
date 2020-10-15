42ac1673dac17bb1f5a0887fd9da1cee45be735e
slTrust <trustfor@sina.cn>
2020-10-15 13:44:26 +0800

    更新切换按钮的样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -10,21 +10,28 @@
         <router-link to="/doc">文档</router-link>
       </li>
     </ul>
-    <span class="toggleAside"
-          @click="toggleMenu"></span>
+    <svg v-if="toggleMenuButtonVisible"
+         class="toggleAside"
+         @click="toggleMenu">
+      <use xlink:href="#icon-menu"></use>
+    </svg>
   </div>
 </template>
 
 <script lang="ts">
 import { inject, Ref } from 'vue'
-
 export default {
+  props: {
+    toggleMenuButtonVisible: {
+      type: Boolean,
+      default: false,
+    },
+  },
   setup() {
     const menuVisible = inject<Ref<boolean>>('menuVisible') // get
     const toggleMenu = () => {
       menuVisible.value = !menuVisible.value
     }
-
     return {
       toggleMenu,
     }
@@ -45,7 +52,6 @@ $color: #007974;
   z-index: 10;
   justify-content: center;
   align-items: center;
-
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -54,40 +60,31 @@ $color: #007974;
       height: 32px;
     }
   }
-
   > .menu {
     display: flex;
     white-space: nowrap;
     flex-wrap: nowrap;
-
     > li {
       margin: 0 1em;
     }
   }
-
-  > .toggleMenu {
-  }
-
   > .toggleAside {
-    display: none;
-    width: 24px;
-    height: 24px;
-    background: red;
+    width: 32px;
+    height: 32px;
     position: absolute;
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
+    display: none;
+    background: fade-out(black, 0.9);
   }
-
   @media (max-width: 500px) {
     > .menu {
       display: none;
     }
-
     > .logo {
       margin: 0 auto;
     }
-
     > .toggleAside {
       display: inline-block;
     }
