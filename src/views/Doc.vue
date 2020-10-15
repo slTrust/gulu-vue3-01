32894b11d23dc47393a596711999069b158c9ce5
slTrust <trustfor@sina.cn>
2020-10-15 14:39:17 +0800

    创建路由

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -4,6 +4,17 @@
             class="nav" />
     <div class="content">
       <aside v-if="menuVisible">
+        <ol>
+          <li>
+            <router-link to="/doc/intro">介绍</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/install">安装</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/get-started">开始使用</router-link>
+          </li>
+        </ol>
         <h2>组件列表</h2>
         <ol>
           <li>
