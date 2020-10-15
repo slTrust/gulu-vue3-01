291cd7cc7ffc9da2ab20ba3352026e33321fb5f8
slTrust <trustfor@sina.cn>
2020-10-15 18:54:35 +0800

    优化 Demo

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,17 +5,21 @@
       <component :is="component" />
     </div>
     <div class="demo-actions">
-      <Button>查看代码</Button>
+      <Button @click="toggleCode">查看代码</Button>
     </div>
     <div class="demo-code">
-      <pre class="language-html"
-           v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')" />
+      <div class="demo-code"
+           v-if="codeVisible">
+        <pre class="language-html"
+             v-html="html" />
+      </div>
     </div>
 
   </div>
 </template>
 
 <script lang="ts">
+import { computed, ref } from 'vue'
 import Button from '../lib/Button.vue'
 import 'prismjs'
 import 'prismjs/themes/prism.css'
@@ -24,9 +28,24 @@ export default {
   props: {
     component: Object,
   },
-  setup() {
+  components: {
+    Button,
+  },
+  setup(props) {
+    const html = computed(() => {
+      return Prism.highlight(
+        props.component.__sourceCode,
+        Prism.languages.html,
+        'html'
+      )
+    })
+    const toggleCode = () => (codeVisible.value = !codeVisible.value)
+    const codeVisible = ref(false)
     return {
       Prism,
+      html,
+      codeVisible,
+      toggleCode,
     }
   },
 }
