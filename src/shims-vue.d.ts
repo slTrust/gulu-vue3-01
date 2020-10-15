bc5d4fd20d0f4bd2adaabecd91e8d827574b4f4f
slTrust <trustfor@sina.cn>
2020-10-15 19:26:53 +0800

    解决上节课的遗留问题

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,11 @@
 declare module '*.vue' {
-    import { ComponentOptions } from 'vue'
-    const ComponentOptions: ComponentOptions
-    export default ComponentOptions
+  import { ComponentOptions } from 'vue'
+  const ComponentOptions: ComponentOptions
+  export default ComponentOptions
+}
+
+/* 告诉ts md文件 以什么形式返回 */
+declare module '*.md' {
+  const str: string
+  export default str
 }
